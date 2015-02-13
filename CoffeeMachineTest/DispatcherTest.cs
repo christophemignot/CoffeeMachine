f6a8fdbbcf6d3c0fe26ec7455b4a1b9357234a53
Christophe Mignot <christophe.mignot@axa.fr>
2015-02-13 18:24:33 +0100

    Add some comment to get back properly

--- CoffeeMachineTest/DispatcherTest.cs
+++ CoffeeMachineTest/DispatcherTest.cs
@@ -6,5 +6,7 @@
     public class DispatcherTest
     {
         //http://stackoverflow.com/questions/15879260/using-moq-how-to-setup-a-method-call-with-an-input-parameter-as-an-object-with
+        //Need to add test on Dispatcher class to check alls calls are made from customer to drinkMachine
+
     }
 }
